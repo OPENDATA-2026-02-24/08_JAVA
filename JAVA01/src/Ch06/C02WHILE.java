bfbf5efe716e88beca74ae8cb1bf3cf6c029517d
jungwoogyun <jwg8910@naver.com>
2026-04-22 15:03:17 +0900

    04 DAY - JAVA01

--- JAVA01/src/Ch06/C02WHILE.java
+++ JAVA01/src/Ch06/C02WHILE.java
@@ -1,5 +1,7 @@
 package Ch06;
 
+import java.util.Scanner;
+
 public class C02WHILE {
 	
 	public static void main(String[] args) {
@@ -19,7 +21,50 @@ public class C02WHILE {
 //		}
 		
 		// N - 9 단 출력
+//		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+//		int dan = n;
+//		while(dan<10) {
+//			
+//			int i=1;
+//			while(i<10) {
+//				System.out.printf("%d x %d = %d\n", dan,i,dan*i);
+//				i++;
+//			}
+//			
+//			System.out.println();
+//			dan++;
+//		}
+//		sc.close();
+		
 		// N - M 단 출력(N<M, N>=2 && N<=9, M>2 && M<10)
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		//유효성검증(N<M, N>=2 && N<=9, M>2 && M<10) 이 아닌경우
+		//(N>=M) ||  (N<2 || N>9) || (M<=2 || M>=10)
+		while((n>=m) ||  (n<2 || n>9) || (m<=2 || m>=10)) {
+			System.out.print("n,m 다시 입력 : ");
+			n = sc.nextInt();
+			m = sc.nextInt();
+		}
+		
+		int dan = n;
+		while(dan<(m+1)) {
+			
+			int i=1;
+			while(i<10) {
+				System.out.printf("%d x %d = %d\n", dan,i,dan*i);
+				i++;
+			}
+			
+			System.out.println();
+			dan++;
+		}
+		sc.close();
+		
 		// N - M 단 출력 거꾸로 2 x 9 , 2 x 8 ....
 		
 		
