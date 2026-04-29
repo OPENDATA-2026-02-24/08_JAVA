7218a982697b2a9a5073b3d650e61d9e233956ab
jungwoogyun <jwg8910@naver.com>
2026-04-29 14:47:32 +0900

    09 DAY  - JAVA02

--- JAVA02/src/Ch07/C02Ex.java
+++ JAVA02/src/Ch07/C02Ex.java
@@ -22,17 +22,58 @@ abstract class Converter {
 
 //원화 -> 달러 
 class Won2Dollar extends Converter{
+
+	public Won2Dollar(double ratio) {
+		this.ratio = ratio;
+	}
+
+	@Override
+	protected double convert(double src) {
+		return src/ratio;
+	}
+
+	@Override
+	protected String getSrcString() {
+		return "원";
+	}
+
+	@Override
+	protected String getDestString() {
+		return "달러";
+	}
 	
 }
 //섭씨 -> 화씨
 class 섭씨to화씨 extends Converter{
+
+	public 섭씨to화씨() {
+
+	}
+
+	@Override
+	protected double convert(double src) {
+		// TODO Auto-generated method stub
+		return (src * 1.8) + 32;
+	}
+
+	@Override
+	protected String getSrcString() {
+		// TODO Auto-generated method stub
+		return "섭씨(℃)";
+	}
+
+	@Override
+	protected String getDestString() {
+		// TODO Auto-generated method stub
+		return "화씨(℉)";
+	}
 	
 }
 public class C02Ex {
 
 	public static void main(String[] args) {
 		//[3번] Converter 클래스를 상속받아 원화를 달러로 변환하는 Won2Dollar 클래스를 작성하라. main() 메소드와 실행 결과는 다음과 같다.
-//		   Won2Dollar toDollar = new Won2Dollar(1399); // 1달러는 1399원
+//		   Won2Dollar toDollar = new Won2Dollar(1477.30); // 1달러는 1399원
 //		   toDollar.run();
 		   
 		   //결과
@@ -40,9 +81,10 @@ public class C02Ex {
 //		   원을 입력하세요>> 24000
 //		   변환 결과: 20.0달러입니다
 
-			//섭씨(℃)화씨(℉) 공식 : 화씨 = (섭씨 × 9/5) + 32
-//		   섭씨to화씨 test = new 섭씨to화씨(); 
-//		   test.run();
+			//섭씨(℃)화씨(℉) 공식 : 화씨 = (섭씨 × 1.8) + 32
+			//
+		   섭씨to화씨 test = new 섭씨to화씨(); 
+		   test.run();
 
 	}
 
