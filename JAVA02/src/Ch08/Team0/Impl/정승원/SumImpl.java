4cb3aa679940f473c1718769152f90cabdd1df4a
jsh340866 <jsh340866@gmail.com>
2026-04-29 17:45:13 +0900

    V0.0 [ 정승원 ] Sum 수정

--- JAVA02/src/Ch08/Team0/Impl/정승원/SumImpl.java
+++ JAVA02/src/Ch08/Team0/Impl/정승원/SumImpl.java
@@ -3,6 +3,48 @@ package Ch08.Team0.Impl.정승원;
 import Ch08.Common.Information;
 import Ch08.Team0.Interfaces.SumInterface;
 
-public class SumImpl {
+public class SumImpl implements SumInterface,Information {
+
+	@Override
+	public void sum(int a, int b) {
+		System.out.println("두 수의 합 : " + (a + b));
+		
+	}
+
+	@Override
+	public void sum(int a, int b, int c) {
+		System.out.println("세 수의 합 : " + (a + b + c));
+		
+	}
+
+	@Override
+	public void sum(int... arg) {
+		int sum = 0;
+		for (int n : arg) {
+			sum += n;
+		}
+		System.out.println("모든 수의 합 : " + sum);
+	}
+
+	@Override
+	public void sum(String... arg) {
+		int sum = 0;
+		for (String s : arg) {
+			sum += Integer.parseInt(s);
+		}
+		System.out.println("모든 문자 수의 합 : " + sum);
+	}
+
+	@Override
+	public String getTeamName() {
+		// TODO Auto-generated method stub
+		return "1조";
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return "정승원";
+	}
 	
 }
