d79cedb617426acece50103b311eaca37978a966
jungwoogyun <jwg8910@naver.com>
2026-05-06 10:00:57 +0900

    11 DAY - JAVA03

---
+++ JAVA03/src/Ch04/C04Ex.java
@@ -0,0 +1,10 @@
+package Ch04;
+
+public class C04Ex {
+	public static void main(String[] args) {
+		//1) 1 ~ 45 까지숫자를 6개를 랜덤으로 받아(Random클래스를이용) set에 저장 / 출력
+		
+		//2) [추가]저장된 set의 오름차순정렬을 해보세요(검색을 통해서 해결해봅니다 - 스트림함수사용 )
+		
+	}
+}	
