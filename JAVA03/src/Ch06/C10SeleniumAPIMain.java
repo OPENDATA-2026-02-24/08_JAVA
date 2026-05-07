aa0e16a98ac41540385e54068ba29d82e9c8d99d
jungwoogyun <jwg8910@naver.com>
2026-05-07 15:06:50 +0900

    12 DAY - JAVA03

---
+++ JAVA03/src/Ch06/C10SeleniumAPIMain.java
@@ -0,0 +1,17 @@
+package Ch06;
+
+public class C10SeleniumAPIMain {
+
+	//크롬서버.jar 다운로드 경로(셀레니움 공식 홈페이지)
+	//https://www.selenium.dev/downloads/
+
+	//크롬드라이브 다운로드 경로
+	//https://googlechromelabs.github.io/chrome-for-testing/#stable
+
+	
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
